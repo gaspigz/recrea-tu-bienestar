8622060079d88699260d2b2eb1372bb69690d466
gaspigz <gaspar.gimenez8@gmail.com>
2026-04-08 16:22:52 -0300

    ver

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -97,14 +97,6 @@ const Pricing = () => {
           </p>
         </div>
 
-
-
-
-
-
-
-
-
   {/* 👇 BOTÓN DE DESCARGA PDF 👇 */}
   <div className="mt-8 flex justify-center w-full">
   { /*<div className="mt-8"> */}
@@ -116,12 +108,11 @@ const Pricing = () => {
         <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
         </svg>
-        Descargar xxx Programa Completo (XXXPNG)
+        Descargar xxx Programa Completo (XXPNG)
       </Button>
     </a>
   </div>
 
-</div>
 
 
 
