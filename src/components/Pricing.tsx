3bf46156d44eb4814691e3c23df945dc7825881f
gaspigz <gaspar.gimenez8@gmail.com>
2026-04-08 16:20:39 -0300

    ver

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -116,7 +116,7 @@ const Pricing = () => {
         <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
         </svg>
-        Descargar xxx Programa Completo (PNG)
+        Descargar xxx Programa Completo (XXXPNG)
       </Button>
     </a>
   </div>
